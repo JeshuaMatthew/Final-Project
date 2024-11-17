0f0897f9a118e0c3f3c93ba2d9282a76923472c9
Jeshua Matthew Hutahaean <2382021@unai.edu>
2024-11-17 08:25:09 +0700

    berhasil menyetup github, database & menyelesaikan layer entites

---
+++ entities/stok.java
@@ -0,0 +1,44 @@
+package entities;
+
+public class stok {
+    private String namaBarang;
+    private int hargaSewa;
+    private int currentStok;
+    private int id;
+
+    public stok() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    public int getHargaSewa() {
+        return hargaSewa;
+    }
+
+    public void setHargaSewa(int hargaSewa) {
+        this.hargaSewa = hargaSewa;
+    }
+
+    public int getCurrentStok() {
+        return currentStok;
+    }
+
+    public void setCurrentStok(int currentStok) {
+        this.currentStok = currentStok;
+    }
+}
