b87b7243b14a97853b145fb8b84ba9cb33cb5ec1
Jeshua Matthew Hutahaean <2381009@unai.edu>
2024-11-28 09:03:15 +0700

    mengubah objek TerminalView di main menjadi MenuView, supaya kriteria penggunaan polymorfism tercapai

--- appSewaKamera/Main.java
+++ appSewaKamera/Main.java
@@ -1,6 +1,7 @@
 package appSewaKamera;
 
 import appSewaKamera.config.Database;
+import appSewaKamera.views.MenuView;
 import appSewaKamera.views.TerminalView;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -12,15 +13,14 @@ public class Main{
     public static void main(String[] args){
 
         ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-        TerminalView terminalView = context.getBean(TerminalView.class);
+        MenuView terminalView = context.getBean(TerminalView.class);
         terminalView.run();
 
-        // jgn lupa hapus sout sampe
     }
 
     @Bean
     Database database (){
-        Database database = new Database("bisnis_sewa_kamera", "root","", "localhost","3306");
+        Database database = new Database("bisnis_penjualan_kamera", "root","", "localhost","3306");
         database.setup();
         return database;
     }
